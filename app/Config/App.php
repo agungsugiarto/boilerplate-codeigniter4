22115b65450cb498db8d6067958331ed8ae3e565
Agung Sugiarto <sugiartoagung92@gmail.com>
2020-04-10 00:27:54 +0700

    change language

--- app/Config/App.php
+++ app/Config/App.php
@@ -63,7 +63,7 @@ class App extends BaseConfig
 	| should run under for this request.
 	|
 	*/
-	public $defaultLocale = 'id';
+	public $defaultLocale = 'en';
 
 	/*
 	|--------------------------------------------------------------------------
