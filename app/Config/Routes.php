10599617af2c6d6e8a3210a7deff01da61fe9660
Agung Sugiarto <sugiartoagung92@gmail.com>
2020-05-11 21:00:30 +0700

    update app

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,7 +1,7 @@
 <?php namespace Config;
 
 // Create a new instance of our RouteCollection class.
-$routes = Services::routes(true);
+$routes = Services::routes();
 
 // Load the system's routing file first, so that the app and ENVIRONMENT
 // can override as needed.
@@ -20,7 +20,7 @@ $routes->setDefaultController('Home');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
-$routes->setAutoRoute(true);
+$routes->setAutoRoute(false);
 
 /**
  * --------------------------------------------------------------------
